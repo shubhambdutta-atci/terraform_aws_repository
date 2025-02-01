cff169e5654c4fca49fdbe7652042e42752cc52c
shubhambdutta-atci <shubham.b.dutta@accenture.com>
2025-02-02 04:02:25 +0530

    Create outputs.tf

---
+++ Day-2/outputs.tf
@@ -0,0 +1,19 @@
+# output "bucket_name" {
+#   description = "The name of the bucket"
+#   value = aws_s3_bucket.lab-1.id
+# }
+
+# output "bucket_arn" {
+#   description = "The Arn of the Lab-1 bucket"
+#   value = aws_s3_bucket.lab-1.arn
+# }
+
+# # output "bucket_policy" {
+# #   description = "The policy of the bucket"
+# #   value = aws_s3_bucket.lab-1.policy
+# # }
+
+# output "bucket_tags" {
+#   description = "The tags involved for the bucket"
+#   value = aws_s3_bucket.lab-1.tags_all
+# }
