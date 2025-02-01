322a275f8e800aa49c8b774a77b9d1ac09423b7e
shubhambdutta-atci <shubham.b.dutta@accenture.com>
2025-02-02 03:59:33 +0530

    Create s3.tf

---
+++ Day2/s3.tf
@@ -0,0 +1,16 @@
+# S3 bucket for lab-1
+resource "aws_s3_bucket" "lab-1" {
+  for_each = toset(var.bucket_name)
+  bucket = each.value
+   tags = merge (
+    var.common_tag,
+    {
+      Name = each.value
+    }
+   )
+}
+
+# tags = {
+  #   Name        = "bucket-demo-lct-lab112192024"
+  #   Environment = "Test"
+  # }
